5e03d253d1740ff265265cebb60aee6880d1e6b1
Xin Liu <ginshio78@gmail.com>
2023-02-09 14:04:10 +0000

    Initial commit

---
+++ setup-basic.sh
@@ -0,0 +1,214 @@
+#!/usr/bin/env bash
+
+# Directories
+mkdir -p $HOME/{Desktop,Documents,Downloads,Music,Pictures,Projects,Public,Templates,Videos,.temp}
+mkdir -p $HOME/.local/{bin,share,lib}
+mkdir -p $HOME/.local/share/{fonts,applications}
+cat <<-EOF |tee $HOME/.config/user-dirs.dirs
+XDG_DESKTOP_DIR="$HOME/Desktop"
+XDG_DOCUMENTS_DIR="$HOME/Documents"
+XDG_DOWNLOAD_DIR="$HOME/Downloads"
+XDG_MUSIC_DIR="$HOME/Music"
+XDG_PICTURES_DIR="$HOME/Pictures"
+XDG_PUBLICSHARE_DIR="$HOME/Public"
+XDG_TEMPLATES_DIR="$HOME/Templates"
+XDG_VIDEOS_DIR="$HOME/Videos"
+EOF
+
+# swap file
+sudo dd if=/dev/zero of=/swapfile bs=4MiB count=16384 status=progress
+sudo chmod 0600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+cat <<-EOF |sudo tee -a /etc/fstab
+/swapfile                                  none       swap  defaults,pri=20  0  0
+EOF
+# sudo sysctl -w vm.swappiness=16
+cat <<-EOF |sudo tee -a /etc/sysctl.conf
+####
+#
+# To disable or override a distribution provided file just place a
+# file with the same name in /etc/sysctl.d/
+#
+# See sysctl.conf(5), sysctl.d(5) and sysctl(8) for more information
+#
+####
+vm.swappiness=10
+EOF
+sudo sysctl -p
+
+# Hosts
+sudo cp /etc/hosts /etc/hosts.bkp
+curl https://raw.githubusercontent.com/ineo6/hosts/master/hosts |sed '1,4d' - |sudo tee -a /etc/hosts
+
+# update source
+### zypper
+sudo zypper rr --all
+sudo zypper ar -fcg https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/oss TUNA:oss
+sudo zypper ar -fcg https://mirrors.tuna.tsinghua.edu.cn/opensuse/tumbleweed/repo/non-oss TUNA:non-oss
+sudo zypper ar -fcg https://mirrors.tuna.tsinghua.edu.cn/packman/suse/openSUSE_Tumbleweed TUNA:packman
+sudo zypper ar -fcg obs://KDE:Extra openSUSE:kDE:Extra
+sudo zypper ar -fcg obs://home:Ximi1970:Mozilla:Add-ons openSUSE:systray-x
+sudo zypper ar -fcg https://download.opensuse.org/repositories/server:/messaging/openSUSE_Factory openSUSE:messaging
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo zypper ar -fcg https://packages.microsoft.com/yumrepos/ms-teams teams
+#sudo zypper ar -fcg https://packages.microsoft.com/yumrepos/edge microsoft-edge
+sudo zypper ar -fcg https://packages.microsoft.com/yumrepos/vscode vscode
+sudo -E zypper ref && sudo zypper dup -y --from TUNA:packman --allow-vendor-change && sudo -E zypper dup -y
+### flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# KDE wallet
+sudo -E zypper in -y pam_kwallet
+cat <<-EOF |sudo tee -a /etc/pam.d/sddm
+auth     optional       pam_kwallet5.so
+session  optional       pam_kwallet5.so auto_start
+EOF
+
+# Common environment
+sudo -E zypper in -y dash fish ripgrep wget curl emacs git git-lfs git-doc \
+    neofetch figlet fcitx5 fcitx5-rime aspell sqlite3 zstd zip unzip 7zip \
+    inkscape ImageMagick graphviz calibre kdeconnect-kde partitionmanager \
+    partitionmanager-lang proxychains-ng privoxy sshpass \
+    mpv amarok obs-studio telegram-desktop spotify-easyrpm osdlyrics osdlyrics-lang
+chsh -s /bin/dash
+
+# C++ environment
+sudo -E zypper in -y gcc gcc-c++ gcc-info gdb binutils-gold gcc7 gcc7-c++ \
+    clang clang-tools lldb lld \
+    cmake extra-cmake-modules ninja ccls ccache \
+    libboost_*-devel poco-devel
+
+# Java environment
+sudo -E zypper in -y java-11-openjdk java-11-openjdk-devel \
+    java-17-openjdk java-17-openjdk-devel
+
+# Python3 environment
+sudo -E zypper in -y python3 python3-virtualenv python3-doc python3-pylint
+sudo pip3 install pyright
+
+# NodeJS environment
+sudo -E zypper in -y nodejs-common yarn
+
+# Beam environment
+sudo -E zypper in -y erlang erlang-doc elixir elixir-doc
+
+# Working dependence
+sudo -E zypper in -y glslang-devel Mesa-libGL-devel wayland-protocols-devel \
+    shaderc vulkan-tools spirv-tools spirv-cross bison flex re2c htcondor
+sudo -E zypper in -y xorg-x11-server-sdk xorg-x11-devel libX11-devel libxcb-devel \
+    libXcomposite-devel libXdamage-devel libXext-devel libXfixes-devel \
+    libXfont-devel libXfont2-devel libXrandr-devel libxshmfence-devel libXxf86vm-devel \
+    libdrm-devel libopenssl-devel wayland-devel waylandpp-devel libelf-devel ncurses5-devel \
+    python3-ruamel.yaml python3-u-msgpack-python python3-pyelftools python3-condor python3-future
+sudo -E zypper in -y teams code MozillaThunderbird systray-x steam
+
+# SSH
+mkdir -p $HOME/.ssh
+cat <<-EOF |tee $HOME/.ssh/config
+# https://docs.github.com/en/authentication/troubleshooting-ssh/using-ssh-over-the-https-port
+Host github github.com
+    Hostname ssh.github.com
+    User git
+    Port 443
+    PreferredAuthentications publickey
+    IdentityFile $HOME/.ssh/amd-pub-git
+
+# https://docs.gitlab.com/ee/user/gitlab_com
+Host gitlab gitlab.com
+    Hostname altssh.gitlab.com
+    User git
+    Port 443
+    PreferredAuthentications publickey
+    IdentityFile $HOME/.ssh/amd-pub-git
+
+Host bitbucket bitbucket.org
+    Hostname altssh.bitbucket.org
+    User git
+    Port 443
+    PreferredAuthentications publickey
+    IdentityFile $HOME/.ssh/amd-pub-git
+EOF
+#ssh-keygen -C "ED25519 Comment" -t ed25519     -f "$HOME/.ssh/amd-pub-git" -N ""
+#ssh-keygen -C "RSA Comment"     -t rsa -b 4096 -f "$HOME/.ssh/amd-pub-git" -N ""
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bkp
+cat <<-EOF |sudo tee /etc/ssh/sshd_config
+Port 22
+Protocol 2
+
+MaxAuthTries 8
+MaxSessions 32
+
+RSAAuthentication no
+PubkeyAuthentication yes
+PermitRootLogin no
+PasswordAuthentication no
+KbdInteractiveAuthentication no
+EOF
+sudo systemctl enable --now sshd.service
+
+# Remote Desktop (don't work)
+sudo -E zypper in -y xrdp krdc
+# x11vnc -storepasswd $HOME/.vnc/passwd
+# cat <<-EOF |sudo tee /etc/systemd/system/x11vnc.service
+# [Unit]
+# Description=X11vnc server
+# Requires=display-manager.service
+# After=display-manager.service
+#
+# [Service]
+# Type=forking
+# ExecStartPre=/bin/bash -c "/usr/bin/systemctl set-environment SDDMXAUTH=\$(/usr/bin/find /var/run/sddm/ -type f)"
+# ExecStart=/bin/bash -c "/usr/bin/x11vnc -display :0 -auth \${SDDMXAUTH} -ncache 10 -forever -loop -shared -bg -rfbauth >
+# #ExecStart=/bin/bash -c "/usr/bin/x11vnc -auth /var/run/sddm/* -display :0 -forever -loop -noxdamage -repeat -rfbauth $>
+# ExecStop=/usr/bin/killall x11vnc
+# Restart=on-failure
+# RestartSec=3
+#
+# [Install]
+# WantedBy=graphical.target
+# EOF
+cat <<-EOF |tee $HOME/.xsession
+\$(dbus-launch --sh-syntax)
+exec /usr/bin/startplasma-x11
+EOF
+sudo systemctl enable --now xrdp.service
+
+# Others
+cd $(mktemp -d)
+### Spotify
+spotify-easyrpm --quiet --set-channel stable --create-schedule
+### Hugo
+curl -o hugo.tar.gz -sSL https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz
+tar -C $HOME/.local/bin -zxvf hugo.tar.gz hugo
+### Perforce
+curl -o p4v.tgz -sSL https://cdist2.perforce.com/perforce/r18.2/bin.linux26x86_64/p4v.tgz
+tar -zxvf p4v.tgz -C $HOME/.local/share
+cat <<-EOF |tee $HOME/.local/share/applications/p4v.desktop
+[Desktop Entry]
+Name=Perforce
+GenericName=Version Control GUI
+Comment=Perforce GUI
+Icon=$HOME/.local/share/p4v-2018.2.1687764/lib/P4VResources/icons/p4v.svg
+Exec=p4v
+Terminal=false
+StartupNotify=false
+Type=Application
+Categories=Development;RevisionControl;
+X-KDE-StartupNotify=false
+EOF
+ln -sf $HOME/.local/share/p4v-2018.2.1687764/bin/p4v $HOME/.local/bin
+update-desktop-database $HOME/.local/share/applications
+### Tex
+sudo -E zypper in -y 'texlive-*'
+### Emacs
+sudo -E zypper in -y libvterm-{tools,devel} libtool
+git clone --depth 1 https://github.com/domtronn/all-the-icons.el.git
+cd all-the-icons.el/fonts
+mkdir -p $HOME/.local/share/fonts/all-the-icons && cp *.ttf $HOME/.local/share/fonts/all-the-icons
+git clone --depth 1 github:doomemacs/doomemacs.git $HOME/.emacs.d
+git clone gitlab:GinShio/doom-private.git $HOME/.doom.d
+emacs --batch --eval "(progn (require 'org) (setq org-confirm-babel-evaluate nil) (org-babel-tangle-file \"$HOME/.doom.d/config.org\"))"
+$HOME/.emacs.d/bin/doom install && $HOME/.emacs.d/bin/doom sync
+curl -o twemoji-v2.tar -sSL "https://raw.githubusercontent.com/iqbalansari/emacs-emojify/9e36d0e8c2a9c373a39728f837a507adfbb7b931/twemoji-fixed-v2.tar"
+mkdir -p $HOME/.emacs.d/.local/cache/emojis && tar -C $HOME/.emacs.d/.local/cache/emojis -xvf twemoji-v2.tar
---
+++ setup-beautify.sh
@@ -0,0 +1,192 @@
+BEAUTIFY_DIR=$(mktemp -d)
+cd $BEAUTIFY_DIR
+mkdir -p $BEAUTIFY_DIR/theme $BEAUTIFY_DIR/icon $BEAUTIFY_DIR/font $BEAUTIFY_DIR/plugin $BEAUTIFY_DIR/sddm $BEAUTIFY_DIR/grub
+
+# Basic
+sudo zypper in -y kvantum-manager kvantum-manager-lang latte-dock
+
+# Themes
+cd $BEAUTIFY_DIR/theme
+function install_theme {
+    local THEME_NAME=$1
+    local THEME_AUTHOR=$2
+    local THEME_REPO=$3
+    local THEME_TAG=$4
+    local THEME_TEMP_DIR=$BEAUTIFY_DIR/theme/$THEME_NAME
+    local THEME_SUBDIR=$5
+    local THEME_PREFIX=$HOME/.local/share
+
+    echo "install THEME: $THEME_NAME ..."
+    mkdir -p $THEME_TEMP_DIR
+    curl -o $THEME_NAME.tar.gz -sSL https://github.com/$THEME_AUTHOR/$THEME_REPO/archive/$THEME_TAG.tar.gz
+    tar -xzf $THEME_NAME.tar.gz -C $THEME_TEMP_DIR
+
+    local theme_components=(aurorae color-schemes knosole konversation kvantum latte-layout plasma sddm wallpapers yakuake)
+    for component in ${theme_components[@]}; do
+        local source_dir=$THEME_TEMP_DIR/$THEME_REPO-$THEME_TAG/$THEME_SUBDIR/$component
+        local target_dir=$THEME_PREFIX/$component
+        mkdir -p $target_dir
+        local subdir=themes
+        case $component in
+            "yakuake")
+                local subdir=skins
+                ;&
+            "aurorae"|"konversation"|"sddm")
+                local source_dir=$source_dir/$subdir
+                [[ "$component" == "sddm" ]] && local target_dir=/usr/share/$component/$subdir || local target_dir=$target_dir/$subdir
+                sudo mkdir -p $target_dir
+                [[ -e $source_dir ]] || local source_dir=$(dirname $source_dir)
+                ;&
+            "color-schemes")
+                [[ "$component" == "color-schemes" && ! -e $source_dir ]] && local source_dir=$(dirname $source_dir)/colorschemes
+                ;&
+            "latte-layout")
+                [[ "$component" == "latte-layout" ]] && local target_dir=$HOME/.config/latte
+                sudo mkdir -p $target_dir
+                ;&
+            "wallpapers")
+                [[ "$component" == "wallpapers" && ! -e $source_dir ]] && local source_dir=$(dirname $source_dir)/wallpaper
+                ;&
+            "kvantum")
+                [[ "$component" == "kvantum" && ! -e $source_dir ]] && local source_dir=$(dirname $source_dir)/Kvantum
+                [[ "$component" == "kvantum" ]] && local target_dir=$HOME/.config/Kvantum
+                sudo mkdir -p $target_dir
+                ;&
+            *)
+                [[ -e $source_dir ]] && sudo cp -R $source_dir/* $target_dir
+                ;;
+        esac
+    done
+}
+install_theme Arc PapirusDevelopmentTeam arc-kde master
+install_theme Dracula dracula gtk master kde
+install_theme Layan vinceliuice Layan-kde master
+install_theme SweetAmbarBule EliverLara Sweet Ambar-Blue kde
+install_theme WhiteSur vinceliuice WhiteSur-kde master
+
+# Icons
+cd $BEAUTIFY_DIR/icon
+ICON_PREFIX=$HOME/.local/share/icons
+mkdir -p $ICON_PREFIX
+function install_icon {
+    local ICON_NAME=$1
+    local ICON_AUTHOR=$2
+    local ICON_REPO=$3
+    local ICON_TAG=$4
+    local ICON_INSTALL=$5
+    local ICON_INSTALL_ARGS=$(echo "$6" |tr ";" "\n")
+    local ICON_ALLDIR=$7
+    local ICON_PREFIXES=$(echo "$8" |tr ";" "\n")
+    local ICON_TEMP_DIR=$BEAUTIFY_DIR/icon/$ICON_NAME
+
+    echo "install ICON & CURSOR: $ICON_NAME ..."
+    mkdir -p $ICON_TEMP_DIR
+    curl -o $ICON_NAME.tar.gz -sSL https://github.com/$ICON_AUTHOR/$ICON_REPO/archive/$ICON_TAG.tar.gz
+    tar -xzf $ICON_NAME.tar.gz -C $ICON_TEMP_DIR
+    local source_dir=$ICON_TEMP_DIR/$ICON_REPO-$ICON_TAG
+
+    pushd $source_dir >/dev/null 2>&1
+    if [ "$ICON_INSTALL" = "true" ]; then
+        bash $source_dir/install.sh ${ICON_INSTALL_ARGS[@]}
+    elif [ "$ICON_ALLDIR" = "true" ]; then
+        mkdir -p $ICON_PREFIX/$ICON_NAME
+        cp -R $source_dir/* $ICON_PREFIX/$ICON_NAME
+    elif [[ ${#ICON_PREFIXES[@]} -ne 0 ]]; then
+        for prefix in ${ICON_PREFIXES[@]}; do
+            cp -R $source_dir/$prefix* $ICON_PREFIX
+        done
+    else
+        cp -R $source_dir/$ICON_NAME* $ICON_PREFIX
+    fi
+    popd >/dev/null 2>&1
+}
+install_icon Qogir vinceliuice Qogir-icon-theme master true
+install_icon Layan vinceliuice Layan-cursors master true
+#install_icon Numix numixproject numix-icon-theme master false
+install_icon Numix-Circle numixproject numix-icon-theme-circle master false
+install_icon Numix-Square numixproject numix-icon-theme-square master false
+install_icon Candy EliverLara candy-icons master false "" true
+install_icon Papirus PapirusDevelopmentTeam papirus-icon-theme master false "" false "Papirus;ePapirus"
+#install_icon Tela vinceliuice Tela-icon-theme master true
+install_icon Deepin zayronxio Deepin-icons-2022 master false "" true
+curl -o Bibata.tar.gz -SL https://github.com/ful1e5/Bibata_Cursor/releases/latest/download/Bibata.tar.gz
+mkdir -p $BEAUTIFY_DIR/icon/Bibata
+tar -xzf Bibata.tar.gz -C $BEAUTIFY_DIR/icon/Bibata
+cp -R $BEAUTIFY_DIR/icon/Bibata/Bibata-Modern-* $ICON_PREFIX
+
+# Fonts
+cd $BEAUTIFY_DIR/font
+### Basic Fonts
+sudo zypper in -y \
+    adobe-{sourceserif4,sourcesans3,sourcecodepro}-fonts \
+    adobe-sourcehanserif-{cn,hk,jp,kr,tw}-fonts \
+    adobe-sourcehansans-{cn,hk,jp,kr,tw}-fonts \
+    wqy-{bitmap,microhei,zenhei}-fonts \
+    fontawesome-fonts
+### opensource Fonts
+curl -o $HOME/.local/share/fonts/SourceHanMono.ttc -sSL https://github.com/adobe-fonts/source-han-mono/releases/download/1.002/SourceHanMono.ttc
+curl -o JuliaMono.tar.gz -sSL https://github.com/cormullion/juliamono/releases/download/v0.047/JuliaMono.tar.gz
+mkdir -p $HOME/.local/share/fonts/Julia-Mono && tar -xzf JuliaMono.tar.gz -C $HOME/.local/share/fonts/Julia-Mono
+curl -o Hasklig.zip -sSL https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hasklig.zip
+unzip Hasklig -d $HOME/.local/share/fonts/Hasklig-Nerd
+curl -o SourceCodeVar.zip -sSL https://github.com/adobe-fonts/source-code-pro/releases/download/2.038R-ro%2F1.058R-it%2F1.018R-VAR/VAR-source-code-var-1.018R.zip
+unzip SourceCodeVar -d $HOME/.local/share/fonts/Source-Code-Variable
+### Microsoft Fonts from github:fphoenix88888/ttf-mswin10-arch
+win10_fonts_langs=("japanese" "korean" "zh_cn" "zh_tw" "sea" "thai" "other")
+curl -o win10-fonts.tar.gz -sSL https://github.com/fphoenix88888/ttf-mswin10-arch/archive/master.tar.gz
+mkdir -p win10-fonts
+tar -xzf win10-fonts.tar.gz -C win10-fonts
+mkdir -p $HOME/.local/share/fonts/win10-english $HOME/.local/share/licenses/win10-fonts
+tar --zstd -xf win10-fonts/ttf-mswin10-arch-master/ttf-ms-win10-10.0.19043.1055-1-any.pkg.tar.zst -C win10-fonts
+mv win10-fonts/usr/share/fonts/TTF/* $HOME/.local/share/fonts/win10-english
+mv win10-fonts/usr/share/licenses/ttf-ms-win10/license.rtf $HOME/.local/share/licenses/win10-fonts/english-fonts-license.rtf
+for lang in ${win10_fonts_langs[@]}; do
+    mkdir -p $HOME/.local/share/fonts/win10-$lang
+    tar --zstd -xf win10-fonts/ttf-mswin10-arch-master/ttf-ms-win10-$lang-10.0.19043.1055-1-any.pkg.tar.zst -C win10-fonts
+    mv win10-fonts/usr/share/fonts/TTF/* $HOME/.local/share/fonts/win10-$lang
+    mv win10-fonts/usr/share/licenses/ttf-ms-win10-$lang/license.rtf $HOME/.local/share/licenses/win10-fonts/$lang-fonts-license.rtf
+done
+
+# Plugins
+cd $BEAUTIFY_DIR/plugin
+sudo zypper in -y applet-window-appmenu applet-window-buttons libQt5WebSockets5 \
+    python3-docopt python3-numpy python3-PyAudio python3-cffi python3-websockets
+function install_plugin {
+    local PLUGIN_NAME=$1
+    local PLUGIN_PLATFORM=$2
+    local PLUGIN_AUTHOR=$3
+    local PLUGIN_REPO=$4
+    local PLUGIN_TAG=$5
+    local PLUGIN_CATEGRAY=$6
+    local PLUGIN_SUBDIR=$7
+    local PLUGIN_TEMP_DIR=$BEAUTIFY_DIR/plugin/$PLUGIN_NAME
+
+    echo "install PLUGIN: $PLUGIN_NAME ..."
+    mkdir -p $PLUGIN_TEMP_DIR
+    case $PLUGIN_PLATFORM in
+        "github")
+            curl -o $PLUGIN_NAME.tar.gz -sSL https://github.com/$PLUGIN_AUTHOR/$PLUGIN_REPO/archive/$PLUGIN_TAG.tar.gz
+            ;;
+        "gitlab")
+            curl -o $PLUGIN_NAME.tar.gz -sSL https://gitlab.com/$PLUGIN_AUTHOR/$PLUGIN_REPO/-/archive/$PLUGIN_TAG/$PLUGIN_REPO-$PLUGIN_TAG.tar.gz
+            ;;
+    esac
+    tar -xzf $PLUGIN_NAME.tar.gz -C $PLUGIN_TEMP_DIR
+    local source_dir=$PLUGIN_TEMP_DIR/$PLUGIN_REPO-$PLUGIN_TAG/$PLUGIN_SUBDIR
+    kpackagetool5 --type Plasma/$PLUGIN_CATEGRAY --install $source_dir
+}
+install_plugin EventCalendar github Zren plasma-applet-eventcalendar master Applet package
+install_plugin LatteSeparator github psifidotos applet-latte-separator master Applet
+install_plugin WindowTitle github psifidotos applet-window-title master Applet
+install_plugin Win11Menu github prateekmedia Menu11 main Applet
+install_plugin ShaderWallpaper github y4my4my4m kde-shader-wallpaper master Wallpaper
+git clone --depth 1 https://github.com/rbn42/panon.git && pushd panon
+git submodule update --depth 1 --init
+cmake -Stranslations -B_build
+DESTDIR=../plasmoid/contents/locale make -f _build/Makefile install
+kpackagetool5 -t Plasma/Applet --install plasmoid
+popd
+curl -o panon-shaders.tar.gz -SL https://github.com/rbn42/panon-effects/archive/master.tar.gz
+mkdir -p panon-shaders $HOME/.config/panon
+tar -xzf panon-shaders.tar.gz -C panon-shaders
+cp -R panon-shaders/panon-effects-master/effects/rbn42-* $HOME/.config/panon
