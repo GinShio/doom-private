d785faa2a3a4a8c36d48a6c1a1aeb12063e7f2a9
Xin Liu <ginshio78@gmail.com>
2024-07-08 14:02:22 +0000

    Update snippet

--- 02-opensuse-setup.sh
+++ 02-opensuse-setup.sh
@@ -50,13 +50,13 @@ sudo -E zypper in -y erlang erlang-doc elixir elixir-doc elixir-hex
 # Working dependence
 sudo -E zypper in -y piglit spirv-tools spirv-cross shaderc vulkan-tools glslang-devel Mesa-libGL-devel Mesa-libRusticlOpenCL
 sudo -E zypper in -y freeglut-devel libdrm-devel libelf-devel libopenssl-devel ncurses5-devel waffle-devel
-sudo -E zypper in -y nanomsg-devel SDL2-devel
 sudo -E zypper in -y xcb-proto-devel xorg-x11-server-sdk libX11-devel libxcb-devel \
     libXcomposite-devel libXdamage-devel libXext-devel libXfixes-devel libXfont-devel \
     libXfont2-devel libxkbcommon-devel libXrandr-devel libxshmfence-devel libXxf86vm-devel
 sudo -E zypper in -y wayland-devel wayland-protocols-devel waylandpp-devel
 sudo -E zypper in -y python3-pyelftools python3-ruamel.yaml python3-u-msgpack-python \
     python3-distutils-extra python3-numpy python3-Mako python3-Jinja2
+sudo -E zypper in -y nanomsg-devel SDL2-devel libglfw-devel stb-devel tinyobjloader-devel
 
 # Virtualization & Containerization & Cross compilation
 sudo -E zypper in -y -t pattern kvm_tools
