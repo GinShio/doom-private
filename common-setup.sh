7982a31720372d7b97e6e6657d5acd32cbd05f99
Xin Liu <ginshio78@gmail.com>
2024-07-22 07:19:03 +0000

    Update snippet

--- common-setup.sh
+++ common-setup.sh
@@ -192,22 +192,7 @@ ccache -M 512G
 cd $(mktemp -d)
 curl -o fisher.fish -SL https://github.com/jorgebucaran/fisher/raw/main/functions/fisher.fish
 fish -C 'source fisher.fish' -c "fisher install jorgebucaran/fisher IlanCosman/tide PatrickF1/fzf.fish"
-cat <<-EOF |fish -c 'tide configure'
-3
-1
-2
-1
-1
-2
-4
-1
-1
-4
-2
-2
-1
-y
-EOF
+fish -c "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Sharp --powerline_prompt_style='Two lines, character' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_spacing=Sparse --icons='Many icons' --transient=No"
 
 # PipX
 pipx install pyright
